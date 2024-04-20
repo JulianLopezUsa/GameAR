928d0024aa5f47f165e7029a8b912971f7b2c155
Laura <laura.garcia07@usa.edu.co>
2024-04-20 14:54:28 -0500

    Se agrega función de llenar bases de datos

--- gamear/src/main/java/com/gamear/gamear/Controlador/UsuarioController.java
+++ gamear/src/main/java/com/gamear/gamear/Controlador/UsuarioController.java
@@ -1,13 +1,19 @@
 package com.gamear.gamear.Controlador;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.gamear.gamear.Usuario;
 import com.gamear.gamear.UsuarioRepository;
 
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.List;
 
 @RestController
@@ -28,6 +34,30 @@ public class UsuarioController {
         usuario.setRol(1); // Asignar rol por defecto
         Usuario nuevoUsuario = usuarioRepository.save(usuario);
         return ResponseEntity.ok(nuevoUsuario);
+    }
+
+    @PostMapping("/upload-csv")
+    public ResponseEntity<String> uploadCSVFile(@RequestParam("file") MultipartFile file) {
+        if (!file.isEmpty()) {
+            try (Reader reader = new InputStreamReader(file.getInputStream())) {
+                Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(reader);
+                for (CSVRecord record : records) {
+                    Usuario usuario = new Usuario();
+                    usuario.setNombre(record.get("nombre"));
+                    usuario.setApellido(record.get("apellido"));
+                    usuario.setCorreo(record.get("correo"));
+                    usuario.setEscuela(record.get("escuela"));
+                    usuario.setRol(Integer.parseInt(record.get("rol")));
+                    usuario.setNumeroCelular(Integer.parseInt(record.get("numeroCelular")));
+                    usuario.setContraseña(record.get("contraseña"));
+                    usuarioRepository.save(usuario);
+                }
+                return ResponseEntity.ok("Usuarios cargados exitosamente!");
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error al cargar el archivo: " + e.getMessage());
+            }
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Archivo vacío");
     }
 
 }
