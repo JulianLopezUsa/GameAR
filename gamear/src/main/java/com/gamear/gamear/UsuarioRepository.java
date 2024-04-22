ee5e56333f9f52ed44626063de0e5c6a9425e813
Carlos Bermudez <106134881+carlosbermudezgalvan@users.noreply.github.com>
2024-04-22 14:12:55 -0500

    Delete gamear/src/main/java/com/gamear/gamear directory

--- gamear/src/main/java/com/gamear/gamear/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gamear.gamear;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findByCorreo(String correo);
-}
