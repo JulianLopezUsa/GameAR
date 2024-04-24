88cfe48e86164391b4538644f4192b9c7910a8cb
Carlos Bermudez <carlos.bermudez01@usa.edu.co>
2024-04-24 08:08:28 -0500

    add scenario repository

---
+++ gamear/src/main/java/com/gamear/gamear/repository/ScenarioRepository.java
@@ -0,0 +1,13 @@
+package com.gamear.gamear.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gamear.gamear.entity.Scenario;
+
+@Repository
+public interface ScenarioRepository extends JpaRepository<Scenario, Long> {
+    List<Scenario> findByName(String name);
+}
