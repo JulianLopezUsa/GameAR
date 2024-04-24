0011b300ff5f994ddbb2462d01ae0f22da8aceb1
Carlos Bermudez <carlos.bermudez01@usa.edu.co>
2024-04-24 09:04:46 -0500

    Add ScenarioService

---
+++ gamear/src/main/java/com/gamear/gamear/service/ScenarioService.java
@@ -0,0 +1,33 @@
+package com.gamear.gamear.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gamear.gamear.entity.Scenario;
+import com.gamear.gamear.repository.ScenarioRepository;
+
+@Service
+public class ScenarioService {
+    @Autowired
+    private ScenarioRepository scenarioRepository;
+
+    public List<Scenario> getAllScenarios() {
+        return scenarioRepository.findAll();
+    }
+
+    public Optional<Scenario> getScenarioById(Long id) {
+        return scenarioRepository.findById(id);
+    }
+
+    public Scenario saveScenario(Scenario scenario) {
+        return scenarioRepository.save(scenario);
+    }
+
+    public void deleteScenario(Long id) {
+        scenarioRepository.deleteById(id);
+    }
+
+}
